e18b131123fa560ae6a7a0f65a4f0685cfaae070
karmb14 <20bcscs008@student.rru.ac.in>
2024-06-04 19:47:03 +0000

    variable is added to 01_basics

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId=14355
+let accountEmail="Karmbhattt@gmail.com"
+var accountPassword="12345"
+accountCity="ahemedabad"
+
+accountEmail="ka@gmail.com"
+accountPassword="123123"
+accountCity="Palanpur"
+// do not use var because of the scoping problem
+console.table([accountId,accountEmail,accountPassword,accountCity])
