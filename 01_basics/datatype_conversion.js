e46e6f0cf29a093f5d0f3945781a2e9f469dc782
karmb14 <20bcscs008@student.rru.ac.in>
2024-06-05 12:31:00 +0000

    datatype conversion done

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,31 @@
+let score="13karm"
+
+console.log(typeof score );
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+let score1= null
+
+console.log(typeof score1 );
+
+let valueInNumber1= Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1);
+
+let score2=undefined
+
+console.log(typeof score2 );
+
+let valueInNumber2= Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2);
+
+let score3=true
+
+console.log(typeof score3 );
+
+let valueInNumber3= Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3);
