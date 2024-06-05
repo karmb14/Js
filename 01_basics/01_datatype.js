82d267356587d15bdf050561a0783af1b088ade3
karmb14 <20bcscs008@student.rru.ac.in>
2024-06-05 12:10:04 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,22 @@
+// alert(3+3) we are using node js not browser
+console.log(3+3)
+
+/*
+Datatypes 
+
+numbers
+bigint
+string
+boolean
+null
+undefined
+symbol
+
+
+object
+
+*/
+
+console.log(typeof "Karm");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
