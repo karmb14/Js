b3deb5a17661208fc7b594ceb3c08fc55d066518
karmb14 <20bcscs008@student.rru.ac.in>
2024-06-07 05:06:48 +0000

    comparison done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)//false
+
+// console.log(null==0)//false
+// console.log(null>0)//false
+// console.log(null>=0)//true
+
+// console.log(undefined==0)//false
+// console.log(undefined>=0)//false
+// console.log(undefined<=0)//false
+
+// === means strict check it not only checks the numbers it checks the datatypes also 
+
+//avoide null and undefined comparisons 
+
+console.log("1"===1)//false
--- 01_basics/datatype_conversion.js
+++ 01_basics/datatype_conversion.js
@@ -1,31 +1,48 @@
-let score="13karm"
+// let score="13karm"
 
-console.log(typeof score );
+// console.log(typeof score );
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
-let score1= null
+// let score1= null
 
-console.log(typeof score1 );
+// console.log(typeof score1 );
 
-let valueInNumber1= Number(score1)
-console.log(typeof valueInNumber1)
-console.log(valueInNumber1);
+// let valueInNumber1= Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1);
 
-let score2=undefined
+// let score2=undefined
 
-console.log(typeof score2 );
+// console.log(typeof score2 );
 
-let valueInNumber2= Number(score2)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2);
+// let valueInNumber2= Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2);
 
-let score3=true
+// let score3=true
 
-console.log(typeof score3 );
+// console.log(typeof score3 );
 
-let valueInNumber3= Number(score3)
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3);
+// let valueInNumber3= Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3);
+
+// //Operations
+
+// let value=3
+// let negValue= -value
+// console.log(negValue);
+
+// let str1="hello"
+// let str2=" Karm"
+
+// let str3= str1 + str2
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+console.log(true);
+console.log(+true);
+console.log(+"");
